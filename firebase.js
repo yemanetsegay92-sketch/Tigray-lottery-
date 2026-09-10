5d75dfbe75356239e8aba5ea7ba9ac5b3bc078ac
yemanetsegay92-sketch <yemanetsegay92@gmail.com>
2026-09-10 21:24:01 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,18 +1,16 @@
-import { initializeApp, getApps, getApp } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC_D-mWZWEhEnfNbx99tmi8svD5smSkmq4',
-  authDomain: 'tigiray-lottery.firebaseapp.com',
-  projectId: 'tigiray-lottery',
-  storageBucket: 'tigiray-lottery.firebasestorage.app',
-  messagingSenderId: '474510516322',
-  appId: '1:474510516322:web:797adc0f38ff54c30ff13b'
+  apiKey: "AIzaSyC_D-mWZWEhEnfNbx99tmi8svD5smSkmq4",
+  authDomain: "tigiray-lottery.firebaseapp.com",
+  projectId: "tigiray-lottery",
+  storageBucket: "tigiray-lottery.firebasestorage.app",
+  messagingSenderId: "474510516322",
+  appId: "1:474510516322:web:797adc0f38ff54c30ff13b"
 };
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export { initializeApp, getAuth };
-export const firebaseConfigExport = firebaseConfig;
