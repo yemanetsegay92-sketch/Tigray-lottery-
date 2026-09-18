43f1416804eb61933acc618d87887ea928f453e0
yemanetsegay92-sketch <yemanetsegay92@gmail.com>
2026-09-18 11:50:43 +0300

    Point buyer Telegram Mini App to final fresh page path

--- api/telegram/_lib.js
+++ api/telegram/_lib.js
@@ -68,7 +68,7 @@ function adminTelegramWebhookUrl() {
 }
 
 function telegramMiniAppUrl() {
-  return 'https://www.tigraylottery.com/telegram-start.html';
+  return 'https://www.tigraylottery.com/telegram-start-v2.html';
 }
 
 function botLink(startParam='') {
